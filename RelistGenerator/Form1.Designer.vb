1a8c776f72c5534c45b17d16aafdd49ac0b16df8
Colin <colin@whitehinge.com>
2017-09-22 15:12:48 +0100

    It's a generator of Relisting CSVs
    Pretty simple little program. 209 lines of code, a good portion of that
    is comments. If things don't make sense, gimme a shout. You click the
    button and it makes you a CSV with its generated name based on the date,
    and puts it in the folder shown in the text box. Click the text box to
    open a folder selector, and when one is picked, it saves it to the
    application settings so you won't have to again. Any issues in it, it
    tells you through the label below the button - bad file names,
    nonexistent folders, preexisting files, etc. If all's good, you click
    the button and it takes 2-3 mins to run a query before automatically
    saving and opening the CSV.

---
+++ RelistGenerator/Form1.Designer.vb
@@ -0,0 +1,151 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class RelistMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RelistMain))
+        Me.FolderLocTXT = New System.Windows.Forms.TextBox()
+        Me.CSVLocationLabel = New System.Windows.Forms.Label()
+        Me.CollectBTN = New System.Windows.Forms.Button()
+        Me.FolderSelection = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ClicktochangeLabel = New System.Windows.Forms.Label()
+        Me.CSVNameLabel = New System.Windows.Forms.Label()
+        Me.CSVNameTXT = New System.Windows.Forms.TextBox()
+        Me.DotCSVLabel = New System.Windows.Forms.Label()
+        Me.StatusLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'FolderLocTXT
+        '
+        Me.FolderLocTXT.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.FolderLocTXT.Location = New System.Drawing.Point(54, 156)
+        Me.FolderLocTXT.Name = "FolderLocTXT"
+        Me.FolderLocTXT.ReadOnly = True
+        Me.FolderLocTXT.Size = New System.Drawing.Size(177, 26)
+        Me.FolderLocTXT.TabIndex = 3
+        Me.FolderLocTXT.Text = "V:\Warehouse\Relist\"
+        '
+        'CSVLocationLabel
+        '
+        Me.CSVLocationLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.CSVLocationLabel.AutoSize = True
+        Me.CSVLocationLabel.Location = New System.Drawing.Point(66, 137)
+        Me.CSVLocationLabel.Name = "CSVLocationLabel"
+        Me.CSVLocationLabel.Size = New System.Drawing.Size(151, 19)
+        Me.CSVLocationLabel.TabIndex = 4
+        Me.CSVLocationLabel.Text = "CSV Export Location"
+        '
+        'CollectBTN
+        '
+        Me.CollectBTN.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.CollectBTN.Font = New System.Drawing.Font("Cambria", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CollectBTN.Location = New System.Drawing.Point(40, 13)
+        Me.CollectBTN.Name = "CollectBTN"
+        Me.CollectBTN.Size = New System.Drawing.Size(205, 49)
+        Me.CollectBTN.TabIndex = 1
+        Me.CollectBTN.Text = "Collect Relist Data"
+        Me.CollectBTN.UseVisualStyleBackColor = True
+        '
+        'ClicktochangeLabel
+        '
+        Me.ClicktochangeLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.ClicktochangeLabel.AutoSize = True
+        Me.ClicktochangeLabel.Location = New System.Drawing.Point(83, 182)
+        Me.ClicktochangeLabel.Name = "ClicktochangeLabel"
+        Me.ClicktochangeLabel.Size = New System.Drawing.Size(112, 19)
+        Me.ClicktochangeLabel.TabIndex = 5
+        Me.ClicktochangeLabel.Text = "Click to change"
+        '
+        'CSVNameLabel
+        '
+        Me.CSVNameLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.CSVNameLabel.AutoSize = True
+        Me.CSVNameLabel.Location = New System.Drawing.Point(96, 213)
+        Me.CSVNameLabel.Name = "CSVNameLabel"
+        Me.CSVNameLabel.Size = New System.Drawing.Size(80, 19)
+        Me.CSVNameLabel.TabIndex = 7
+        Me.CSVNameLabel.Text = "CSV Name"
+        '
+        'CSVNameTXT
+        '
+        Me.CSVNameTXT.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.CSVNameTXT.Location = New System.Drawing.Point(54, 232)
+        Me.CSVNameTXT.Name = "CSVNameTXT"
+        Me.CSVNameTXT.Size = New System.Drawing.Size(141, 26)
+        Me.CSVNameTXT.TabIndex = 6
+        Me.CSVNameTXT.Text = "relist-yyMMdd"
+        '
+        'DotCSVLabel
+        '
+        Me.DotCSVLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.DotCSVLabel.AutoSize = True
+        Me.DotCSVLabel.Location = New System.Drawing.Point(195, 235)
+        Me.DotCSVLabel.Name = "DotCSVLabel"
+        Me.DotCSVLabel.Size = New System.Drawing.Size(34, 19)
+        Me.DotCSVLabel.TabIndex = 8
+        Me.DotCSVLabel.Text = ".csv"
+        '
+        'StatusLabel
+        '
+        Me.StatusLabel.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.StatusLabel.Location = New System.Drawing.Point(12, 61)
+        Me.StatusLabel.Name = "StatusLabel"
+        Me.StatusLabel.Size = New System.Drawing.Size(260, 70)
+        Me.StatusLabel.TabIndex = 9
+        Me.StatusLabel.Text = "This will take approx. 5 minutes"
+        Me.StatusLabel.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'RelistMain
+        '
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.StatusLabel)
+        Me.Controls.Add(Me.DotCSVLabel)
+        Me.Controls.Add(Me.CSVNameLabel)
+        Me.Controls.Add(Me.CSVNameTXT)
+        Me.Controls.Add(Me.ClicktochangeLabel)
+        Me.Controls.Add(Me.CSVLocationLabel)
+        Me.Controls.Add(Me.FolderLocTXT)
+        Me.Controls.Add(Me.CollectBTN)
+        Me.Font = New System.Drawing.Font("Cambria", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(300, 300)
+        Me.MinimizeBox = False
+        Me.MinimumSize = New System.Drawing.Size(300, 300)
+        Me.Name = "RelistMain"
+        Me.Text = "Relist Generator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents FolderLocTXT As TextBox
+    Friend WithEvents CSVLocationLabel As Label
+    Friend WithEvents CollectBTN As Button
+    Friend WithEvents FolderSelection As FolderBrowserDialog
+    Friend WithEvents ClicktochangeLabel As Label
+    Friend WithEvents CSVNameLabel As Label
+    Friend WithEvents CSVNameTXT As TextBox
+    Friend WithEvents DotCSVLabel As Label
+    Friend WithEvents StatusLabel As Label
+End Class
---
+++ RelistGenerator/Form1.vb
@@ -0,0 +1,209 @@
+﻿Imports WHLClasses
+Imports System.IO
+
+Public Class RelistMain
+    ''' <summary>
+    ''' Global query data storage
+    ''' </summary>
+    Dim querydata As New List(Of Dictionary(Of String, Object))
+
+    ''' <summary>
+    ''' On Loading, we want to ensure we can gather info from the server, and check the V drive exists
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub RelistMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Check Server is receiving requests.
+        If My.Computer.Network.Ping("sql") Then
+            'Good, we're working. Load the default folder from settings.
+            FolderLocTXT.Text = My.Settings.DefaultFolderLoc
+            'Now set the CSV Automatic Name
+            CSVNameTXT.Text = "relist-" + Today.ToString("yyMMdd")
+            'Check folder
+            If Not My.Computer.FileSystem.DirectoryExists(FolderLocTXT.Text) Then
+                'Well, we need a folder to save in.
+                StatusLabel.Text = "The folder being referenced doesn't exist or can't be reached. Please choose another folder."
+            End If
+        Else
+            'We can't get info. That basically means that something is up with the server as a whole.
+            MsgBox("Failed to connect to the MySQL server. Please contact technical support.")
+            'May as well just not open at all. They can reopen it when they can connect to the server.
+            Me.Close()
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' SQL stuff - This is where we get our info for the table.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CollectBTN_Click(sender As Object, e As EventArgs) Handles CollectBTN.Click
+
+        'Check filename is valid
+        If CSVNameTXT.Text.IndexOfAny(Path.GetInvalidFileNameChars()) >= 0 Then
+            'We have invalid characters
+            StatusLabel.Text = "There are invalid characters in the file name. Please remove them before continuing."
+        ElseIf Not My.Computer.FileSystem.DirectoryExists(FolderLocTXT.Text) Then
+            'Folder doesn't exist
+            StatusLabel.Text = "The folder being referenced doesn't exist or can't be reached. Please choose another folder."
+        ElseIf CSVNameTXT.Text.Trim = "" Then
+            'No name is here
+            StatusLabel.Text = "Please enter a file name."
+        ElseIf My.Computer.FileSystem.FileExists(FolderLocTXT.Text + CSVNameTXT.Text + ".csv") Then
+            'File exists already.
+            StatusLabel.Text = "That file exists already. Please choose another name or rename / remove the old file."
+        Else
+            'Ok, go!
+            'Visual Effect; This will take a while, a signal is nice.
+            StatusLabel.Text = "Please wait."
+            StatusLabel.ForeColor = Color.Maroon
+
+            'Query
+            Dim QueryText As String = "SELECT
+    a.shortsku,
+    d.ShelfName as 'Location',
+    c.Title,
+    CONCAT('\'',b.SupplierCode) as 'SupplierCode',
+    CAST(c.stock AS SIGNED) as 'Level',
+    CAST(c.stockminimum AS SIGNED) as 'Min',
+    (c.stock + c.stockminimum) as 'Total'
+FROM whldata.whlnew as a
+LEFT JOIN whldata.sku_supplierdata as b on a.shortsku=b.SKU
+LEFT JOIN whldata.inventory as c on a.sku=c.sku
+LEFT JOIN
+		(SELECT
+			qa.shortsku,
+			qa.Shelfname,
+            qa.LocationID,
+            qb.LocType,
+            qb.LocID
+		FROM whldata.shortsku_locations as qa
+        LEFT JOIN whldata.locationreference as qb on qa.LocationID=qb.LocID
+        WHERE
+        qb.LocType=1 Group by qa.shortsku)
+	as d on a.shortsku=d.shortsku
+WHERE
+(CAST(c.stock AS SIGNED) < CAST(c.stockminimum AS SIGNED)) AND (CAST(c.stockminimum AS SIGNED) >= 3) AND (b.isPrimary = 'True') AND NOT (a.packsize > c.stockminimum) AND (a.islisted = 'True');"
+
+            'Get Info
+            querydata.Clear()
+            Try
+                'If this works, and gets info, we're golden. Otherwise... well, we need to tell them.
+                querydata = MySQL.SelectDataDictionary(QueryText)
+            Catch ex As Exception
+                'I wanna know about it.
+                Reporting.ErrorReporting.ReportException(ex, False, False)
+                StatusLabel.Text = "An error in data retrieval has been reported to the developer."
+                Exit Sub
+            End Try
+
+            If Not querydata.Count > 0 Then
+                'They need to know they got nothing rather than just... sitting around waiting for the table to fill when it isnt going to.
+                StatusLabel.Text = "No relist data was received. See technical support."
+                Exit Sub
+            Else
+                'Begin saving
+                CSVSavingSetup()
+            End If
+
+            'Visual effect; rather than continuing to say Please Wait...
+            StatusLabel.Text = "Saved."
+            StatusLabel.ForeColor = SystemColors.ControlText
+        End If
+    End Sub
+
+    ''' <summary>
+    ''' Once data is present, we can export it to a CSV file.
+    ''' </summary>
+    Private Sub CSVSavingSetup()
+        'The file name is usually in the format of 'relist-yyMMdd'.
+        Dim FileName As String = FolderLocTXT.Text + CSVNameTXT.Text + ".csv"
+
+        'Now we do the saving thing! This requires a worker to do the stuff for us, but we exit the loop now.
+        OperationDialog("Generating CSV", AddressOf CSVExport, FileName, True)
+    End Sub
+
+    ''' <summary>
+    ''' This code is Lee's CSV export code in Inventory Control, only modified slightly. It works.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub CSVExport(sender As System.ComponentModel.BackgroundWorker, e As System.ComponentModel.DoWorkEventArgs)
+        'Setup
+        Dim rawcsv As String = ""
+
+        'Report to user
+        sender.ReportProgress(0, "Preparing CSV")
+        'Get our column names, seperate by comma
+        rawcsv += String.Join(",", querydata(0).Keys)
+        'Primary save - We continue adding to this later
+        My.Computer.FileSystem.WriteAllText(e.Argument, rawcsv, False)
+        'Report to user
+        sender.ReportProgress(0, "Generating CSV")
+
+        'More setup
+        Dim DispPercentage As Integer = 0
+        rawcsv = ""
+
+        'Get every row of data
+        For Each row As Dictionary(Of String, Object) In querydata
+            'Display completion percentage
+            DispPercentage += 1
+            sender.ReportProgress((DispPercentage / querydata.Count) * 100)
+            'CSV moves down a line.
+            rawcsv += vbNewLine
+
+            'Whenever we reach this threshold, we save and clear the cache.
+            'One would think reaching this limit wouldn't happen often.
+            'On 20/09/17, it triggered 3 times on a single run. Some of those item titles are huge.
+            If rawcsv.Length > 10240 Then
+                'Escaping the symbols
+                rawcsv = rawcsv.Replace("\", "\\").Replace("""", """""").Replace("," + vbNewLine, vbNewLine).Replace("§", """")
+                'Mid-progress saving; Append
+                My.Computer.FileSystem.WriteAllText(e.Argument, rawcsv, True)
+                'Clear text
+                rawcsv = ""
+            End If
+
+            'For everything in the dictionary
+            For Each value In row.Values
+                'Formatting exists, but it has a problem - human error. If the text we're saving contains certain symbols, we need to prevent formatting issues.
+                If value.ToString.Contains(",") Or value.ToString.Contains("""") Or value.ToString.Contains(vbNewLine) Or value.ToString.StartsWith(" ") Or value.ToString.EndsWith(" ") Then
+                    rawcsv += "§" + value.ToString.Replace(vbNewLine, "") + "§,"
+                Else
+                    rawcsv += value.ToString.Replace(vbNewLine, "") + ","
+                End If
+            Next
+        Next
+
+        'Escaping the symbols
+        rawcsv = rawcsv.Replace("\", "\\").Replace("""", """""").Replace("," + vbNewLine, vbNewLine).Replace("§", """")
+        'Report completion
+        sender.ReportProgress(100, "Saving file")
+        'Final save; Append
+        My.Computer.FileSystem.WriteAllText(e.Argument, rawcsv, True)
+
+        'Load the file
+        Process.Start(e.Argument)
+    End Sub
+
+    ''' <summary>
+    ''' We want easy default folder selection
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub FolderLocTXT_Click(sender As Object, e As EventArgs) Handles FolderLocTXT.Click
+        'Show the dialog, only act if they click the OK button
+        If FolderSelection.ShowDialog = DialogResult.OK Then
+            'Change text to reflect changes
+            FolderLocTXT.Text = FolderSelection.SelectedPath
+            'Errorproofing - it needs that slash at the end, or it'll save as Documentsrelist-yyMMdd in the folder above or something.
+            If Not FolderLocTXT.Text.EndsWith("\") Then
+                FolderLocTXT.Text = FolderLocTXT.Text + "\"
+            End If
+            'Then save that to the settings.
+            My.Settings.DefaultFolderLoc = FolderLocTXT.Text
+            My.Settings.Save()
+        End If
+    End Sub
+End Class
